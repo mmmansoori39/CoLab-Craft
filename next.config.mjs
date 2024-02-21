9868d6c8993f25fdc04becd50b05d75c9389f2e0
Md Moinuddin Mansoori <mdmoinuddinmansoori@gmail.com>
2024-02-21 09:35:16 +0530

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "liveblocks.io",
+        port: "",
+      },
+    ],
+  },
+
+  webpack(config, { isServer }) {
+    config.module.rules.push({
+      test: /\.node$/,
+      use: 'file-loader',
+    });
+
+    return config;
+  },
+};
 
 export default nextConfig;
